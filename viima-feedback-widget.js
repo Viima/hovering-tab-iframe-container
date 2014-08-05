7f7926f1c8b876b2a7d7f99ddc1afbfd6cb3034f
Jesse Nieminen <jesse.nieminen@relex.fi>
2014-08-05 20:21:25 +0300

    Got rid of jQuery. The library now requires no external resources.

--- viima-feedback-widget.js
+++ viima-feedback-widget.js
@@ -29,84 +29,57 @@ function initiateViima(viimaId, tabImgUrl, heightFactor, widthFactor, tabOffsetF
   var tabOffset = tabOffsetFromBottom * window.innerHeight;
 
   //Appending tab to body
-  $('body').append('<div id="feedback-tab"><img src="'+ tabImgUrl +'" alt="Feedback"/><div id="feedback-inner"><iframe src="https://app.viima.com/' + viimaId + '"></iframe></div></div>');
+  document.body.insertAdjacentHTML('beforeend', '<div id="feedback-tab"><img src="'+ tabImgUrl +'" alt="Feedback"/><div id="feedback-inner"><iframe src="https://app.viima.com/' + viimaId + '"></iframe></div></div>');
 
-  //Setting CSS
-	$('#feedback-tab').css({
-          'position': 'fixed',
-           'bottom':  tabOffset + 'px',
-           'left': '0',
-           'cursor': 'pointer',
-           '-webkit-transition-duration': '0.3s',
-           '-moz-transition-duration': '0.3s',
-           '-o-transition-duration': '0.3s',
-           'transition-duration': '0.3s',
-           'z-index': '10000'
-  });
-
-
-  $('#feedback-inner').css({
-          'position': 'fixed',
-          'bottom': containerOffset + 'px',
-          'left': '-' + width + 'px',
-          '-webkit-transition-duration': '0.3s',
-          '-moz-transition-duration': '0.3s',
-          '-o-transition-duration': '0.3s',
-          'transition-duration': '0.3s',
-          'height': height + 'px',
-          'width': width + 'px',
-          'overflow': 'hidden',
-          'z-index': '10000'
-  });
-
-  $('#feedback-inner iframe').css({
-          'height': '100%',
-          'width': '100%',
-          'z-index': '10001'
-  });
+  //Setting Basic CSS
+  document.getElementById("feedback-tab").style.cssText = 'position: fixed; bottom: ' + tabOffset + 'px; left: 0; cursor: pointer; -webkit-transition-duration: 0.3s; -moz-transition-duration: 0.3s; -o-transition-duration: 0.3s; transition-duration: 0.3s; z-index: 10000;';
+  document.getElementById("feedback-inner").style.cssText = 'position: fixed; bottom: ' + containerOffset + 'px; left: -' + width + 'px; -webkit-transition-duration: 0.3s; -moz-transition-duration: 0.3s; -o-transition-duration: 0.3s; transition-duration: 0.3s;  height: ' + height + 'px; width: ' + width +'px; overflow: hidden; z-index: 10000;';
+  document.getElementById("feedback-inner").getElementsByTagName('iframe')[0].style.cssText = 'height: 100%; width: 100%; z-index: 10001;';
 
   //Extra style settings
   if(borderColor){
-    $('#feedback-inner iframe').css('border', '3px solid ' + borderColor);
+    document.getElementById('feedback-inner').getElementsByTagName('iframe')[0].style.border = '3px solid ' + borderColor;
   };
 
   if(tabOpacity){
-    $('#feedback-tab img').css('opacity', tabOpacity);
+     document.getElementById('feedback-tab').getElementsByTagName('img')[0].style.opacity = tabOpacity;
   }
 
   if(customClassForTab){
-    $('#feedback-tab').addClass(customClassForTab);
+    document.getElementById('feedback-tab').classList.add(customClassForTab);
   }
 
-  $('#feedback-tab').click(toggleViima);
+  document.getElementById('feedback-tab').addEventListener('click', toggleViima);
 
   function toggleViima(){
     //If the feedback tab is hidden, show it, otherwise hide it
-    if($('#feedback-inner').css('left').indexOf('-') != -1){
-      $('#feedback-tab').css('left', width + 'px');
-      $('#feedback-tab #feedback-inner').css('left', '0px');
-      $('#feedback-tab #feedback-inner').css('bottom', containerOffset + 'px');
-      $('body').append('<div id="feedback-backdrop" style="position: absolute; top: 0; bottom: 0; right: 0; left: 0; z-index: 9999; background-color: #000; opacity: 0.7;"></div>');
+    if(document.getElementById('feedback-inner').style.left.indexOf('-') != -1){
+      document.getElementById('feedback-tab').style.left = width + 'px';
+      document.getElementById('feedback-tab').style.bottom = containerOffset + 'px';
+      document.getElementById('feedback-inner').style.left = '0px';
+      document.getElementById('feedback-inner').style.bottom = containerOffset + 'px';
+      document.body.insertAdjacentHTML('beforeend', '<div id="feedback-backdrop" style="position: absolute; top: 0; bottom: 0; right: 0; left: 0; z-index: 9999; background-color: #000; opacity: 0.7;"></div>');
     } else {
       closeViima();
     }
   }
 
   function closeViima() {
-    $('#feedback-tab').css('left', '0px');
-    $('#feedback-tab #feedback-inner').css('left', '-' + width + 'px');
-    $('#feedback-backdrop').remove();
+    if(document.getElementById('feedback-inner').style.left.indexOf('-') == -1){
+      document.getElementById('feedback-tab').style.left =  '0px';
+      document.getElementById('feedback-inner').style.left = '-' + width + 'px';
+      var backdrop = document.getElementById('feedback-backdrop');
+      document.body.removeChild(backdrop);
+    }
   }
 
   //Hide the feedback widget on clicks outside the container
-  $(document).mouseup(function (e)
+  document.addEventListener('mouseup', (function (e)
   {
-      var container = $('#feedback-tab');
-
-      if (!container.is(e.target) // if the target of the click isn't the container...
-          && container.has(e.target).length === 0) // ... nor a descendant of the container
-      {
+      var backdrop = document.getElementById('feedback-backdrop');
+       // if the target of the click is outside the container, it has to be the backdrop..
+      if (e.target === backdrop){
           closeViima();
       }
-  });
+  }));
 }
